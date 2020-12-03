b611582f69b5fcf74d2e8f981dbc1ccd72226dda
PereSalty <davidballard46000@gmail.com>
2020-12-03 16:54:01 +0100

    Finition problème, début controler

--- src/controleur/Main.java
+++ src/controleur/Main.java
@@ -1,13 +1,29 @@
 package controleur;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
+
+import java.io.*;
 
 public class Main extends Application {
     public static Parent root;
     @Override
-    public void start(Stage stage) throws Exception {
-        
+    public void start(Stage primaryStage) throws Exception {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("../fxml/Menu.fxml"));
+            primaryStage.setTitle("My Application");
+            primaryStage.setScene(new Scene(root));
+            primaryStage.show();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public static void main(String[] args) {
+        launch(args);
     }
 }
--- src/controleur/MenuControleur.java
+++ src/controleur/MenuControleur.java
@@ -1,12 +1,35 @@
 package controleur;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class MenuControleur implements Initializable {
+    @FXML
+    Button btnMenuPers;
+    @FXML
+    Button btnQuitterApp;
+    @FXML
+    Button btnListeNiveau;
+
     @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
+    public void initialize(URL url, ResourceBundle resourceBundle){
+
+    }
+    public void affichageListeNiveau(){
+
+    }
+    public void affichagePers(){
+
+    }
+    public void quitterApp(){
+        // get a handle to the stage
+        Stage stage = (Stage) btnQuitterApp.getScene().getWindow();
+        // do what you have to do
+        stage.close();
     }
 }
