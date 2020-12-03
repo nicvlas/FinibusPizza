ffbe2b5481578c69aa75232a9979f5d8b99f46c7
PereSalty <davidballard46000@gmail.com>
2020-12-03 17:11:15 +0100

    Changement de page réussi dans Main

--- src/controleur/MenuListeNiveauControleur.java
+++ src/controleur/MenuListeNiveauControleur.java
@@ -1,13 +1,45 @@
 package controleur;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class MenuListeNiveauControleur implements Initializable {
+    @FXML
+    Button btnListNivClass;
+    @FXML
+    ComboBox comboListNivClass;
+    @FXML
+    Button btnListNivPerso;
+    @FXML
+    ComboBox comboListNivPerso;
+    @FXML
+    Button btnRetourMenu;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        
+
     }
+
+    public void retourMenu(){
+
+    }
+
+    public void validationChoixListNivPerso(){
+
+    }
+    public void choixListNivPerso(){
+
+    }
+    public void ListNivClassChoix(){
+
+    }
+    public void validationChoixNivClass(){
+
+    }
+
 }
