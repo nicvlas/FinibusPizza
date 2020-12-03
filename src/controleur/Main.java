db819510a9e805946f4c7018aff8c74316de02a2
PereSalty <davidballard46000@gmail.com>
2020-12-03 16:04:56 +0100

    main de test

---
+++ src/controleur/Main.java
@@ -0,0 +1,13 @@
+package controleur;
+
+import javafx.application.Application;
+import javafx.scene.Parent;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    public static Parent root;
+    @Override
+    public void start(Stage stage) throws Exception {
+        
+    }
+}
--- src/controleur/MenuControleur.java
+++ src/controleur/MenuControleur.java
@@ -8,6 +8,5 @@ import java.util.ResourceBundle;
 public class MenuControleur implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-
     }
 }
